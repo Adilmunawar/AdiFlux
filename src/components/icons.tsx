1a750cd70f437209590bae23fb024260b7e6f5fe
Adil Munawar <adilfromavengers@gmail.com>
2025-08-10 07:49:31 +0000

    make it more advance

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,12 +9,14 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2L2 22M12 2L22 22M7 15H17" />
+      <path d="M12 2L2 7L12 12L22 7L12 2Z" />
+      <path d="M2 17L12 22L22 17" />
+      <path d="M2 12L12 17L22 12" />
     </svg>
   );
 }
