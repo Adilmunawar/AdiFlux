e76f88b92648f8e038f0917b27d51a198c11815b
Adil Munawar <adilfromavengers@gmail.com>
2025-08-13 08:54:48 +0000

    still I'm facing thses issues [13:50:27.001] Running build in Washington

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,10 +1,10 @@
 // IMPORTANT: This file is used to expose the Genkit flows as API endpoints.
 // It should not be modified, as it will be managed by the Genkit CLI.
-import {nextHandler} from '@genkit-ai/next';
+import next from '@genkit-ai/next';
 import '@/ai/flows/suggest-style-prompt';
 import '@/ai/flows/generate-image-flow';
 import '@/ai/flows/upscale-prompt-flow';
 import '@/ai/flows/upscale-image-flow';
 import '@/ai/flows/edit-image-flow';
 
-export const POST = nextHandler();
+export const POST = next();
